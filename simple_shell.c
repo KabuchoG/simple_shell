0d625961bd6f7a90a94d9e25f46a196b64082daf
Calvin Simiyu <siimiyucalvin9@gmail.com>
2022-08-12 05:44:13 -0700

    Another commit

--- simple_shell.c
+++ simple_shell.c
@@ -1,7 +1,14 @@
 #include "shell.h"
 
 /* global variable for ^C handling */
+unsigned int sig_flag;
 
+/**
+ * sig_handler - handles ^C signal interupt
+ * @uuv: unused variable (required for signal function prototype)
+ *
+ * Return: nothing
+ */
 static void sig_handler(int uuv)
 {
 	(void) uuv;
